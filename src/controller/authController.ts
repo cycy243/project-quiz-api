fd460fde7f09ea15361730ca3b30413f067b0439
Cyril Auquier <=>
2024-07-31 21:13:38 +0200

    get file from request

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -1,40 +1,36 @@
-import { Controller, Param, Body, Get, Post, Put, Delete, JsonController } from 'routing-controllers';
+import { Body, Post, JsonController, UploadedFile } from 'routing-controllers';
 import IUserService from '../services/iUserService';
 import { Inject, Service } from 'typedi';
-import { UserDto } from '../dto/userDto';
 import { InjectionKey } from '../utils/injection_key';
+import { File } from 'koa-multer';
+import { UserDto } from '../dto/userDto';
 
 @Service()
-@JsonController('/users')
-export class UserController {
+@JsonController('/auth')
+export class AuthController {
     private _service: IUserService
 
     constructor(@Inject(InjectionKey.USER_SERVICE) service: IUserService) {
         this._service = service
     }
 
-  @Get('')
-  getAll() {
-    return 'This action returns all users';
-  }
-
-  @Get('/:id')
-  getOne(@Param('id') id: number) {
-    return 'This action returns user #' + id;
-  }
-
-  @Post('')
-  async post(@Body() user: UserDto) {
+    /**
+     * Create a post route
+     * 
+     * @param file  File to upload (we get the file with the "UploadedFile" annotation and it is put in the file variable)
+     * @param user  users data
+     * 
+     * @returns     The registered user
+     */
+  @Post()
+  async register(@UploadedFile('file') file: File, @Body() user: UserDto) {
+    console.log("coucou");
+    
+    console.log("User: " + JSON.stringify(user));
+    console.log("file: " + file.originalname);
+    console.log(user.name);
+    console.log(user.birthDate);
+       
     return await this._service.addUser(user);
-  }
-
-  @Put('/:id')
-  put(@Param('id') id: number, @Body() user: any) {
-    return 'Updating a user...';
-  }
-
-  @Delete('/:id')
-  remove(@Param('id') id: number) {
-    return 'Removing user...';
   }
 }
