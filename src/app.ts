d4b1a8edcb06f325e387dcc67506729fcd006b89
Cyril Auquier <=>
2024-07-31 21:13:06 +0200

    remove unused items

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import { useContainer, useExpressServer } from "routing-controllers";
-import { UserController } from "./controller/authController";
 import "reflect-metadata";
 import Container from "typedi";
 import { IUserRepository } from "./repository/iUserRepository";
@@ -7,20 +6,16 @@ import { UserRepository } from "./repository/mongo_repository/userRepository";
 import User from "./models/user";
 import IUserService from "./services/iUserService";
 import UserService from "./services/implementations/userService";
-import { Validator } from "fluentvalidation-ts";
-import { UserDto } from "./dto/userDto";
 import UserCUValidator from "./validator/userCUValidator";
 import { InjectionKey } from "./utils/injection_key";
 import { HttpErrorHandler } from "./middlewares/errorsMiddleware";
+import multer from "multer";
 
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
 
 Container.set<IUserRepository>(InjectionKey.USER_REPOSITORY, new UserRepository(User))
 Container.set<UserCUValidator>(InjectionKey.USER_CRUD_VALIDATOR, new UserCUValidator())
