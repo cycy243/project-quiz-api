aad36a436474f8204ed00df9189113936173557a
Cyril Auquier <=>
2024-08-30 20:15:13 +0200

    add connect user logic

--- src/services/iUserAuthService.ts
+++ src/services/iUserAuthService.ts
@@ -1,3 +1,4 @@
+import { ConnectUserDto } from "../dto/auth/connectUserDto";
 import { RegisterUserDto } from "../dto/auth/registerUserDto";
 import { UserDto } from "../dto/userDto";
 
@@ -5,4 +6,13 @@ export default interface IUserAuthService {
 
     registerUser(dto: RegisterUserDto): Promise<UserDto | null>
 
+    /**
+     * Connect the user
+     * 
+     * @param login     User's mail or pseudo
+     * @param password  User's account password
+     * 
+     * @returns a promise containing either the user data (with a token for the connection) or null, if the connection wasn't successfull
+     */
+    connectUserWithLogin(dto: ConnectUserDto): Promise<UserDto | null>
 }
--- src/services/implementations/userAuthService.ts
+++ src/services/implementations/userAuthService.ts
@@ -9,19 +9,27 @@ import RegisterUserValidator from "../../validator/registerUserValidator";
 import { ValidationError } from "../../errors/validationError";
 import { RegisterUserDto } from "../../dto/auth/registerUserDto";
 import IFileSaverService from "../iFileSaverService";
-import { getFilesRootFolder } from "../../librairies/paths";
+import * as bcrypt from 'bcrypt';
 import { join, resolve } from "path";
+import { IUser } from "../../models/user";
+import NoUserFoundError from "../errors/noUserFoundError";
+import { ConnectUserDto } from "../../dto/auth/connectUserDto";
+import ITokenGenerator from "../utils/iTokenGenerator";
+
+const HASH_SALT = 10;
 
 @Service(InjectionKey.USER_SERVICE)
 export default class UserAuthService implements IUserAuthService {
     private _repository: IUserRepository
     private _crudValidator: RegisterUserValidator
     private _fileSaverService: IFileSaverService
+    private _tokenGenerator: ITokenGenerator
 
-    constructor(@Inject(InjectionKey.USER_REPOSITORY) repository: IUserRepository, @Inject(InjectionKey.USER_CRUD_VALIDATOR) crudValidator: RegisterUserValidator, @Inject(InjectionKey.FILE_SAVE_SERVICE) fileSaverService: IFileSaverService) {
+    constructor(@Inject(InjectionKey.USER_REPOSITORY) repository: IUserRepository, @Inject(InjectionKey.USER_CRUD_VALIDATOR) crudValidator: RegisterUserValidator, @Inject(InjectionKey.FILE_SAVE_SERVICE) fileSaverService: IFileSaverService, @Inject(InjectionKey.TOKEN_GENERATOR_SERVICE) tokenGenerator: ITokenGenerator) {
         this._repository = repository
         this._crudValidator = crudValidator
         this._fileSaverService = fileSaverService
+        this._tokenGenerator = tokenGenerator
     }
 
     async registerUser(dto: RegisterUserDto): Promise<UserDto | null> {
@@ -36,9 +44,28 @@ export default class UserAuthService implements IUserAuthService {
         }
         const filePath = resolve(`./public/files/imgs/avatars`)
         
-        //  the img folder will be served under the path 'img' instead of 'public/file/img'
+        dto.password = bcrypt.hashSync(dto.password || "", HASH_SALT)
         const conversion = {...dto, profilePicUri: 'img/avatars/' + this._fileSaverService.saveFileToPath(dto.avatar!, filePath, `${dto.pseudo}-${new Date().getUTCMilliseconds()}`) }
         const result = (await this._repository.insert(userMapper.toEntity(conversion)))!
         return userMapper.toDto(result)
+    }
+
+    async connectUserWithLogin(dto: ConnectUserDto): Promise<UserDto | null> {
+        if(!dto.login) {
+            throw new ValidationError("A validation error occured", [ "User must provide a login" ]);
+        }
+        if(!dto.password) {
+            throw new ValidationError("A validation error occured", [ "User must provide a password" ]);
+        }
+        const user: IUser | null = await this._repository.findBy({ email: dto.login, pseudo: dto.login });
+        if(!user) {
+            throw new NoUserFoundError("No user were found for the given mail or pseudo")
+        }
+        if(!bcrypt.compareSync(dto.password, user.password.toString())) {
+            throw new NoUserFoundError("Wrong password")
+        }
+        
+        let resultDto: UserDto = userMapper.toDto(user)
+        return { ...resultDto, token: (await this._tokenGenerator.generateToken(resultDto))! }
     }
 }
